d4e335b21e88b45ff31ec0e9f3adaa2a38c14745
Jake Kaad <kaadalac@gmail.com>
2015-05-14 13:36:52 -0700

    Add leap year

--- leap_year_junit/src/test/java/LeapYearTest.java
+++ leap_year_junit/src/test/java/LeapYearTest.java
@@ -1,15 +1,29 @@
-package LeapYear;
-
 import org.junit.*;
 import static org.junit.Assert.*;
 
 public class LeapYearTest {
 
   @Test
-  public void checkIfLeapYearTest() {
+  public void isLeapYear_forNumberDivisibleByFour_true() {
     LeapYear leapYear = new LeapYear();
+    assertEquals( true, leapYear.isLeapYear(2012) );
+  }
 
-    assertEquals( true, leapYear.checkIfLeapYear(2000) );
-    assertEquals( false, leapYear.checkIfLeapYear(2001) );
+  @Test
+  public void isLeapYear_forNumbersNotDivisibleByFour_false() {
+    LeapYear leapYear = new LeapYear();
+    assertEquals( false, leapYear.isLeapYear(1999) );
+  }
+
+  @Test
+  public void isLeapYear_forMultiplesOfOneHundred_false() {
+    LeapYear leapYear = new LeapYear();
+    assertEquals( false, leapYear.isLeapYear(1900) );
+  }
+
+  @Test
+  public void isLeapYear_forMultiplesOfFourHundred_true() {
+    LeapYear leapYear = new LeapYear();
+    assertEquals( true, leapYear.isLeapYear(2000) );
   }
 }
